747cc92d255d4936f32a1ab56a6d1984f9dea230
Chary-cake <theodoros16dtbm@gmail.com>
2026-05-23 19:43:53 +0100

    start FrozenStarts lib

--- build/CMakeFiles/FrozenStars.dir/src/main.cpp.o.ddi.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/FrozenStars.dir/src/main.cpp.o.ddi: \
-  /home/chery/Documents/FrozenStars/src/main.cpp
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chery/Documents/FrozenStars
-# Build directory: /home/chery/Documents/FrozenStars/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
